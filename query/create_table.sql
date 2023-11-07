1cf1d8aa65881e698c19bef23066e0eb91b9b967
Rico Mardiansyah <119834647+ricomrdnsyh@users.noreply.github.com>
2023-11-07 12:00:16 +0700

    Create create_table.sql

---
+++ query/create_table.sql
@@ -0,0 +1,37 @@
+-- create table pelanggan
+CREATE TABLE pelanggan(
+    id_pelanggan CHAR(50) PRIMARY KEY NOT NULL,
+    nama VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    no_hp VARCHAR(20) NOT NULL,
+    alamat VARCHAR(100)
+);
+
+-- create table kategori
+CREATE TABLE kategori (
+    id_kategori CHAR(50) NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+-- create table produk
+CREATE TABLE produk(
+    id_produk CHAR(50) PRIMARY KEY NOT NULL,
+    nama VARCHAR(50) NOT NULL,
+    kategori_id CHAR(50) NOT NULL,
+    harga FLOAT NOT NULL,
+    stok INT(5) NOT NULL,
+    Foreign Key (kategori_id) REFERENCES kategori(id_kategori)
+);
+
+-- create table transaksi
+CREATE TABLE transaksi(
+    id_transaksi CHAR(50) NOT NULL PRIMARY KEY,
+    pelanggan_id CHAR(50) NOT NULL,
+    produk_id CHAR(50) NOT NULL,
+    tgl_transaksi DATE NOT NULL,
+    jumlah INT(5) NOT NULL,
+    total_harga FLOAT NOT NULL,
+    Foreign Key (pelanggan_id) REFERENCES pelanggan(id_pelanggan),
+    Foreign Key (produk_id) REFERENCES produk(id_produk)
+);
