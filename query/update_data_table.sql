77b41ff80f6a49afbb93a64113fd5db1746e322f
Rico Mardiansyah <119834647+ricomrdnsyh@users.noreply.github.com>
2023-11-07 13:56:42 +0700

    Create update_data_table.sql

---
+++ query/update_data_table.sql
@@ -0,0 +1,5 @@
+-- update harga pada tabel produk
+UPDATE produk SET harga = 150000 WHERE id_produk = 'prd-4';
+
+-- update nama dan email pada tabel pelanggan
+UPDATE pelanggan SET nama = 'Rico Mardiansyah', email = 'ricomardiansyah27@gmail.com' WHERE id_pelanggan = 'plg-3';
