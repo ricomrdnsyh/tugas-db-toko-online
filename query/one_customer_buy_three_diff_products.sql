d030ab4bbcc1ac7506167c32b3b9bb09465c5f0b
Rico Mardiansyah <119834647+ricomrdnsyh@users.noreply.github.com>
2023-11-09 11:29:23 +0700

    Create one_customer_buy_three_diff_products.sql

---
+++ query/one_customer_buy_three_diff_products.sql
@@ -0,0 +1,4 @@
+INSERT INTO transaksi (id_transaksi, pelanggan_id, produk_id, tgl_transaksi, jumlah, total_harga) VALUES
+('trns-11', 'plg-1', 'prd-2', '2023-10-08', 1, 300000),
+('trns-12', 'plg-1', 'prd-1', '2023-10-08', 2, 150000),
+('trns-13', 'plg-1', 'prd-5', '2023-10-08', 4, 140000);
